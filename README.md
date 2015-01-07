29a541d1438d7a5511bbf2989f2d07a6ca058cc5
pwldp <pawlakdp@gmail.com>
2015-01-07 14:15:19 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ovpn-log-parser
 NodeJs OpenVPN log parser
+
+
+
+Module based on: http://quickleft.com/blog/creating-and-publishing-a-node-js-module
---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * Read and parse OpenVPN status log and exposes it in JSON.
+ *
+ * @param  {String} pathToOvpnLogFile
+ * @return {JSON}
+ */
+module.exports = {
+    parseOvpnLog: function( pathToOvpnLogFile ) {
+	return {};
+    }
+}
